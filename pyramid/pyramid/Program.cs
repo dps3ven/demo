e20e86383b2f5e373ef5e47480d7a6b87b327c80
dpeterson <dpitpro@gmail.com>
2019-07-02 18:56:37 -0500

    initial commit

---
+++ pyramid/pyramid/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k, l, rows;
+            Console.Write("Input rows: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = rows - i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                    
+            
+                for (k = 1; k < i; k++)
+                {
+                    Console.Write("{0}", k);
+                }
+
+                for (l = i; l > 0; l--)
+                {
+                    Console.Write("{0}", l);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
